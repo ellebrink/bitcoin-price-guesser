95836a67227dc9cff87e8dd83a8eca6f990b1134
Gabriel Ellebrink <code.ngat4@simplelogin.co>
2026-02-09 13:51:34 +0100

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-white mb-4">
+          Bitcoin Price Guesser
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
